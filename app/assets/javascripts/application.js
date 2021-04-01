24915c3fdd2d9bda5a87cad7d0b5ef5704a6fa0c
Darren <darren.guzman15@gmail.com>
2021-04-01 15:46:47 -0400

    Add CSS to the navigation bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,12 @@
 //= require jquery
 //= require bootstrap-sprockets
+
+// Bootstrap
+@import "bootstrap-sprockets";
+@import "bootstrap";
+
+// Variables
+@import "base/variables";
+
+// Partials - main css files
+@import "partials/layout/*";
