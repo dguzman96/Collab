a4e146230d4b80a682abf301a06f3918dd54303e
Darren <darren.guzman15@gmail.com>
2021-05-20 15:44:33 -0400

    Define specs for Group::Conversations and Messages

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,13 @@ Rails.application.routes.draw do
 
   resources :contacts, only: [:create, :update, :destroy]
 
+  namespace :group do
+    resources :conversations do
+      member do
+        post :close
+        post :open
+      end
+    end
+    resources :messages, only: [:index, :create]
+  end
 end
