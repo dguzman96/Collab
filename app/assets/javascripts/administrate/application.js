f952d122eb7c7b4e4acbcaaa94378043a20cdebb
Darren <darren.guzman15@gmail.com>
2021-05-27 12:15:09 -0400

    Added collapsible links file - updated application.html.erb file

---
+++ app/assets/javascripts/administrate/application.js
@@ -0,0 +1,14 @@
+//= require jquery
+//= require bootstrap-sprockets
+
+// Bootstrap
+@import "bootstrap-sprockets";
+@import "bootstrap";
+
+// Variables
+@import "base/variables";
+
+// Partials - main css files
+@import "partials/layout/*";
+
+@import 'babel-polyfill';
